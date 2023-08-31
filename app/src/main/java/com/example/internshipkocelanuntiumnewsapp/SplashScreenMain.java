700e9193e6f17d3c243f3b242a24c3093f04a23e
Kev Ngetich <ngetichkevin67@gmail.com>
2023-08-31 09:23:09 +0300

    Change to Splash Screen to open Onboarding activity

--- app/src/main/java/com/example/internshipkocelanuntiumnewsapp/SplashScreenMain.java
+++ app/src/main/java/com/example/internshipkocelanuntiumnewsapp/SplashScreenMain.java
@@ -19,7 +19,7 @@ public class SplashScreenMain extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent =new Intent(SplashScreenMain.this,MainActivity.class);
+                Intent intent =new Intent(SplashScreenMain.this,Onboarding.class);
                 startActivity(intent);
                 finish();
 
