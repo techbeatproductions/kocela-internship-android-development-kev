70126f092fcfaa8c8ec62e9680315fc4bc7b7a68
Kev Ngetich <ngetichkevin67@gmail.com>
2023-09-01 01:03:09 +0300

    Articles Activity

--- app/src/main/java/com/example/internshipkocelanuntiumnewsapp/NewsActivity.java
+++ app/src/main/java/com/example/internshipkocelanuntiumnewsapp/NewsActivity.java
@@ -2,13 +2,29 @@ package com.example.internshipkocelanuntiumnewsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewsActivity extends AppCompatActivity {
 
+    Button newsBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        newsBtn = (Button) findViewById(R.id.random_news_button);
+        onClickListeners();
+    }
+
+    private void onClickListeners(){
+        newsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NewsActivity.this, articles.class));
+            }
+        });
     }
 }
