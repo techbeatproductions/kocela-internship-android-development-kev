4c55788718d636b27f8065ebdf9bf6b152d02c50
Kev Ngetich <ngetichkevin67@gmail.com>
2023-09-01 10:58:00 +0300

    Fix activities switching

--- app/src/main/java/com/example/internshipkocelanuntiumnewsapp/SignIn.java
+++ app/src/main/java/com/example/internshipkocelanuntiumnewsapp/SignIn.java
@@ -6,63 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class SignIn extends AppCompatActivity {
 
     Button signIn,googleSignIn,faceBookSignIn;
     TextView forgotPassword;
+    LinearLayout signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        signIn= (Button) findViewById(R.id.signinbutton);
-
-        googleSignIn=(Button) findViewById(R.id.signinwithgooglebutton);
-        faceBookSignIn=(Button) findViewById(R.id.signinwithfacebookbutton);
-        forgotPassword=(TextView) findViewById(R.id.forgot_password_clickable_txt) ;
 
 
 
 
-        OnClickListeners();
+
+
 
     }
 
-    private void OnClickListeners(){
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SignIn.this, select_your_favorite_topics.class));
-            }
+    public void OnClickListeners(View view){
+        if(view.getId() == R.id.signinbutton){
+           startActivity(new Intent(SignIn.this, select_your_favorite_topics.class));
+
+        } else if (view.getId() == R.id.signinwithgooglebutton) {
+            startActivity(new Intent(SignIn.this, select_your_favorite_topics.class));
+
+        } else if (view.getId() == R.id.signinwithfacebookbutton ) {
+            startActivity(new Intent(SignIn.this, select_your_favorite_topics.class));
+
+        } else if (view.getId() == R.id.forgot_password_clickable_txt) {
+            startActivity(new Intent(SignIn.this, Forgot_Password.class));
+
+        } else if (view.getId() == R.id.sign_in_sign_up_redirect) {
+            startActivity(new Intent(SignIn.this, SignUp.class));
 
 
-        });
-
-        googleSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SignIn.this, select_your_favorite_topics.class));
-            }
-
-
-        });
-
-        faceBookSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SignIn.this, select_your_favorite_topics.class));
-            }
-
-
-        });
-
-        forgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SignIn.this, Forgot_Password.class));
-            }
-        });
+        }
 
 
     }
--- app/src/main/java/com/example/internshipkocelanuntiumnewsapp/select_your_favorite_topics.java
+++ app/src/main/java/com/example/internshipkocelanuntiumnewsapp/select_your_favorite_topics.java
@@ -9,92 +9,23 @@ import android.widget.Button;
 
 public class select_your_favorite_topics extends AppCompatActivity {
 
-    Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt10;
+    Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt10,nextBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_your_favorite_topics);
 
-        bt1 = (Button) findViewById(R.id.sports_topic_button);
-        bt2 = (Button) findViewById(R.id.politics_topic_button);
-        bt3 = (Button) findViewById(R.id.life_topic_button);
-        bt4 = (Button) findViewById(R.id.gaming_topic_button);
-        bt5 = (Button) findViewById(R.id.animals_topic_button);
-        bt6 = (Button) findViewById(R.id.nature_topic_button);
-        bt7 = (Button) findViewById(R.id.food_topic_button);
-        bt8 = (Button) findViewById(R.id.art_topic_button);
-        bt9 = (Button) findViewById(R.id.history_topic_button);
-        bt10 = (Button) findViewById(R.id.fashion_topic_button);
+        nextBtn = (Button) findViewById(R.id.next_fav_topic_button);
+
+
 
         onClickListeners();
     }
 
     private void onClickListeners(){
 
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(select_your_favorite_topics.this, NewsActivity.class));
-            }
-        });
 
-        bt2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(select_your_favorite_topics.this, NewsActivity.class));
-            }
-        });
-
-        bt3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(select_your_favorite_topics.this, NewsActivity.class));
-            }
-        });
-
-        bt4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(select_your_favorite_topics.this, NewsActivity.class));
-            }
-        });
-
-        bt5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(select_your_favorite_topics.this, NewsActivity.class));
-            }
-        });
-
-        bt6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(select_your_favorite_topics.this, NewsActivity.class));
-            }
-        });
-
-        bt7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(select_your_favorite_topics.this, NewsActivity.class));
-            }
-        });
-
-        bt8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(select_your_favorite_topics.this, NewsActivity.class));
-            }
-        });
-
-        bt9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(select_your_favorite_topics.this, NewsActivity.class));
-            }
-        });
-
-        bt10.setOnClickListener(new View.OnClickListener() {
+        nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(select_your_favorite_topics.this, NewsActivity.class));
